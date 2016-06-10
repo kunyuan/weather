03e4be9a64afbb7a93ca81f04f9c9c4d89c45a99
Kun Chen <chenkun@mail.ustc.edu.cn>
2016-06-09 23:02:57 -0400

    add cron jobs

--- weather.py
+++ weather.py
@@ -4,6 +4,7 @@ import forecastio
 import requests
 import json
 import time
+import os
 
 def main():
     """
@@ -14,31 +15,32 @@ def main():
     j = json.loads(r.text)
     lat = j['latitude']
     lon = j['longitude']
-    print j, lat, lon
+    # print j, lat, lon
 
     api_key = "b6235f6d81011107eeedb7ac0e124019"
 
     forecast = forecastio.load_forecast(api_key, lat, lon)
-
-    print "===========Currently Data========="
-    print forecast.currently()
-    print forecast.currently().time
-    print forecast.currently().time+datetime.timedelta(seconds=-time.timezone)
 
     #take Daylight Saving Time (DST) into account
     is_dst = time.daylight and time.localtime().tm_isdst > 0
     utc_offset = - (time.altzone if is_dst else time.timezone)
 
     print forecast.currently().time+datetime.timedelta(seconds=utc_offset)
+    print "weather condition: ", forecast.currently().summary
+    # os.system("/home/osmc/weather/codesend 3554818 -l 285")
+    if "cloudy" in forecast.currently().summary:
+        print "Send Turn On"
+        os.system("/home/osmc/weather/codesend 3554824 -l 285")
 
-    print forecast.currently().summary
-
-    print "===========Hourly Data========="
-    by_hour = forecast.hourly()
-    print "Hourly Summary: %s" % (by_hour.summary)
-
-    for hourly_data_point in by_hour.data:
-        print hourly_data_point
+    # outlet 1
+    # "on" => 3554824,
+    # "off" => 3554822
+    # outlet 2
+    # "on" => 3554818,
+    # "off" => 3554817
+    # outlet 3
+    # "on" => 3554829,
+    # "off" => 3554819
 
     # print "===========Daily Data========="
     # by_day = forecast.daily()
